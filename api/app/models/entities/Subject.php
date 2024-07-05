21e32343269b6e2273dcc18caa305ecf815e5913
phichitchai <phichitchai.pch@gmail.com>
2024-07-05 18:50:19 +0700

    change credit to unit

--- api/app/models/entities/Subject.php
+++ api/app/models/entities/Subject.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Subject extends Model {
     protected $table = 'subject'; // Table name
     protected $primaryKey = 'subject_id'; // Primary key column
-    protected $fillable = ['subject_id', 'course_of_study_id', 'name', 'credit', 'type']; // Fillable columns
+    protected $fillable = ['subject_id', 'course_of_study_id', 'name', 'unit', 'type']; // Fillable columns
     protected $hidden = ['created_at', 'updated_at', 'status'];
     public $timestamps = true;
     public $incrementing = false; // If primary key is not auto-incrementing
